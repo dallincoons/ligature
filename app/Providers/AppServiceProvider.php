21c899a5dea37744241e38e6d5d8904e4455b920
Dallin <dallinis@hotmail.com>
2017-10-07 11:37:00 -0700

    includes namespace

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Gateways\Crawler;
 use App\Gateways\GoutteCrawler;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
